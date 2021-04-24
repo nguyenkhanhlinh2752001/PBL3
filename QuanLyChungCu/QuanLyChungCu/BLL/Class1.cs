a71b70f6b0bf12879a80ff0d45621f574889b118
whoknow2001 <huytran161201@gmail.com>
2021-04-24 11:29:08 +0700

    huy

--- QuanLyChungCu/QuanLyChungCu/BLL/Class1.cs
+++ QuanLyChungCu/QuanLyChungCu/BLL/Class1.cs
@@ -8,10 +8,8 @@ namespace QuanLyChungCu.BLL
 {
     class Class1
     {
-<<<<<<< HEAD:QuanLyChungCu/QuanLyChungCu/BLL/ABC.cs
         int a { get; set; }
         int b { get; set; }
-=======
->>>>>>> bf5558390b94a705c4058165d389a2bc93d0ac58:QuanLyChungCu/QuanLyChungCu/BLL/Class1.cs
+
     }
 }
