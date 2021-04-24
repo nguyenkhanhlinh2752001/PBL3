3df4b4828cb78f9042380c7c93ecb4599d1578fd
whoknow2001 <huytran161201@gmail.com>
2021-04-24 10:12:13 +0700

    asdsa

--- QuanLyChungCu/QuanLyChungCu/View/Form_QuanLy.Designer.cs
+++ QuanLyChungCu/QuanLyChungCu/View/Form_QuanLy.Designer.cs
@@ -43,7 +43,6 @@
             this.lb_Tieude = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -244,15 +243,6 @@
             this.panel2.Size = new System.Drawing.Size(691, 70);
             this.panel2.TabIndex = 5;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(647, 185);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // Form_QuanLy
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -260,7 +250,6 @@
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(1191, 788);
             this.ControlBox = false;
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -294,6 +283,5 @@
         private System.Windows.Forms.Button btn_help;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
     }
 }
