6270c390f5bf6b5c5d07434477c82344b6634166
Kim Ngoc <hoangkimngoc44@gmail.com>
2021-04-26 23:02:39 +0700

    them button trong form cho thue

--- QuanLyChungCu/QuanLyChungCu/View/Form_ChoThue.Designer.cs
+++ QuanLyChungCu/QuanLyChungCu/View/Form_ChoThue.Designer.cs
@@ -32,6 +32,7 @@ namespace QuanLyChungCu.View
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -61,11 +62,21 @@ namespace QuanLyChungCu.View
             this.label3.TabIndex = 2;
             this.label3.Text = "label3";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(419, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form_ChoThue
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -82,5 +93,6 @@ namespace QuanLyChungCu.View
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
